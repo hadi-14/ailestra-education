dab22a28da03ba7e4b8b033d252897ab8d579eed
hadi-14 <hadimillwala@gmail.com>
2024-11-12 17:18:33 +0500

    fixed link isssue

--- src/app/StudentPortal/page.tsx
+++ src/app/StudentPortal/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from 'next/image'
-import Link from 'next/link'
+// import Link from 'next/link'
 import Footer from '../components/footer';
 import NavBarStudentPortal from './components/header';
 import { useState, useRef } from 'react';
